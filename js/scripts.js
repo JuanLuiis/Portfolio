7af0af7d15a2ea30d8ee89e7bf7f2feda936a712
Juan Luis <96555380+JuanLuiis@users.noreply.github.com>
2021-12-24 03:19:44 +0100

    Delete js directory

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-// -- Esto inicializara AOS library --
-AOS.init();
-
-let menu = document.getElementById('menu');
-
-let menu_bar = document.getElementById('menu-bar');
-
-menu_bar.addEventListener('click', () => menu.classList.toggle('menu-toggle'))
