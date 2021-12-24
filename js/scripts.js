0d55f2756bfe041ef3317500682e9f736646f561
Juan Luis <96555380+JuanLuiis@users.noreply.github.com>
2021-12-24 03:20:26 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* Esto inicializara AOS library */
+
+AOS.init();
+
+let menu = document.getElementById('menu');
+
+let menu_bar = document.getElementById('menu-bar');
+
+menu_bar.addEventListener('click', () => menu.classList.toggle('menu-toggle'))
