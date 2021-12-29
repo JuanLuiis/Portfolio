7f47b7c6e1073c3b2f30aa3f475c58d7bdb18575
Juan Luis <96555380+JuanLuiis@users.noreply.github.com>
2021-12-29 16:26:50 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ let menu = document.getElementById('menu');
 let menu_bar = document.getElementById('menu-bar');
 
 menu_bar.addEventListener('click', () => menu.classList.toggle('menu-toggle'))
+
+
+
+const button = document.querySelector('.skills');
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    const desplegable = document.querySelector('.lenguajes');
+    desplegable.classList.remove('no-mostrar');
+})
